6fa922fdf834a1eca2e7527962a1be38f8642ff5
harsha89 <harsz89@gmail.com>
2016-04-25 01:00:59 +0530

    adding further upates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 function goToWelcome() {
     //window.location.href = "https://open-ic.epic.com/argonaut/oauth2/authorize?response_type=code&client_id=e1920551-0f59-464d-a707-7daa5a112be5&redirect_uri=http://localhost/hackthon-app"
-    window.location.href = 'welcome.html';
+    var userId = $('#userName').val();
+    window.location.href = 'welcome.html?userId='+userId;
     return true;
 };
+
+$( document ).ready(function() {
+    var userId = getParameterByName('userId', window.location.href);
+    $('#welcomeUser').text('Welcome ' + userId);
+});
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
