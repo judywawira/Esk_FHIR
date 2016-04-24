1d3f8b4db5da043349de9fd71a1f05b0945bb5a1
harsha89 <harsz89@gmail.com>
2016-04-25 01:20:29 +0530

    updates:

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function goToWelcome() {
     //window.location.href = "https://open-ic.epic.com/argonaut/oauth2/authorize?response_type=code&client_id=e1920551-0f59-464d-a707-7daa5a112be5&redirect_uri=http://localhost/hackthon-app"
     var userId = $('#userName').val();
-    window.location.href = 'welcome.html?userId='+userId;
+    window.location.href = 'patient.html?userId='+userId;
     return true;
 };
 
