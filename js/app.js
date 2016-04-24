ada944e9bf3a9f68e8004a2c19973413a0433ecc
harsha89 <harsz89@gmail.com>
2016-04-24 23:04:17 +0530

    adding styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-function getAuthCode() {
+function goToWelcome() {
     //window.location.href = "https://open-ic.epic.com/argonaut/oauth2/authorize?response_type=code&client_id=e1920551-0f59-464d-a707-7daa5a112be5&redirect_uri=http://localhost/hackthon-app"
     window.location.href = 'welcome.html';
+    return true;
 };
